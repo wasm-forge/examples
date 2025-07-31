d8c76de08f8898dfe1c4fa06efe55485d111d21f
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-07-31 13:57:30 +0200

    add convenience methods

--- ic-rusqlite/src/ic-rusqlite-backend/build.rs
+++ ic-rusqlite/src/ic-rusqlite-backend/build.rs
@@ -3,8 +3,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-
-    // locate wasi builtins
+    // locate WASI builtins
     let sdk_path = if let Ok(sdk_path) = env::var("WASI_SDK") {
         sdk_path
     } else {
@@ -12,7 +11,7 @@ fn main() {
     };
 
     let pattern = format!("{sdk_path}/lib/clang/*/lib/wasip1");
-    
+
     let paths: Vec<PathBuf> = glob(&pattern)
         .expect("Failed to read glob pattern")
         .filter_map(Result::ok)
@@ -24,7 +23,6 @@ fn main() {
         println!("cargo:rustc-link-search={}", path.display());
         println!("cargo:rustc-link-arg=-lclang_rt.builtins-wasm32");
     } else {
-        panic!("Could not find clang wasm32 builtins under '{}'", pattern);
+        panic!("Could not find clang wasm32 builtins under '{pattern}'");
     }
-
 }
--- ic-rusqlite/src/ic-rusqlite-backend/src/lib.rs
+++ ic-rusqlite/src/ic-rusqlite-backend/src/lib.rs
@@ -1,199 +1,35 @@
 use std::cell::RefCell;
+use std::collections::HashMap;
+use std::ops::Range;
 
 use candid::CandidType;
 use candid::Deserialize;
-use rusqlite::types::Type;
 use rusqlite::Connection;
-use rusqlite::ToSql;
 
 use ic_stable_structures::memory_manager::MemoryId;
 use ic_stable_structures::{memory_manager::MemoryManager, DefaultMemoryImpl};
 
+const FS_MOUNT_RANGE: Range<u8> = 200..210;
+const DEFAULT_MOUNTED_DB_ID: u8 = 20;
+const DB_DEFAULT_FILE_NAME: &str = "db.db3";
+
+// re-export some of the core dependencies for others to use
+pub use ic_wasi_polyfill;
+pub use rusqlite;
+
 thread_local! {
-    static DB: RefCell<Option<Connection>> = RefCell::new(None);
-    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
-        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
-}
+    pub static DB: RefCell<Option<Connection>> = const { RefCell::new(None) };
 
-const MOUNTED_MEMORY_ID: u8 = 20;
-const DB_FILE_NAME: &str = "db.db3";
+    pub static DB_FILE_NAME: RefCell<Option<String>> = const { RefCell::new(None) };
 
-#[ic_cdk::update]
-fn add(name: String, data: String, age: u32) {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-        db.execute(
-            "INSERT INTO person (name, data, age) VALUES (?1, ?2, ?3)",
-            (&name, &data, age),
-        )
-        .unwrap();
-    });
-}
+    pub static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = {
+        let m = MemoryManager::init(DefaultMemoryImpl::default());
 
-#[ic_cdk::query]
-fn list() -> Vec<(u64, String, String, u32)> {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-        let mut stmt = db
-            .prepare("SELECT id, name, data, age FROM person")
-            .unwrap();
-        let rows = stmt
-            .query_map([], |row| {
-                Ok((
-                    row.get(0).unwrap(),
-                    row.get(1).unwrap(),
-                    row.get(2).unwrap(),
-                    row.get(3).unwrap(),
-                ))
-            })
-            .unwrap();
-        let mut result = vec![];
-        for person in rows {
-            result.push(person.unwrap());
-        }
-        result
-    })
-}
+        // initialize ic-wasi-polyfill
+        ic_wasi_polyfill::init_with_memory_manager(&[0u8; 32], &[], &m, FS_MOUNT_RANGE);
 
-#[ic_cdk::query]
-fn query(sql: String) -> QueryResult {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-
-        let mut stmt = db.prepare(&sql).unwrap();
-        let cnt = stmt.column_count();
-        let mut rows = stmt.query([]).unwrap();
-        let mut res: Vec<Vec<String>> = Vec::new();
-
-        loop {
-            match rows.next() {
-                Ok(row) => match row {
-                    Some(row) => {
-                        let mut vec: Vec<String> = Vec::new();
-                        for idx in 0..cnt {
-                            let v = row.get_ref_unwrap(idx);
-                            match v.data_type() {
-                                Type::Null => vec.push(String::from("")),
-                                Type::Integer => vec.push(v.as_i64().unwrap().to_string()),
-                                Type::Real => vec.push(v.as_f64().unwrap().to_string()),
-                                Type::Text => vec.push(v.as_str().unwrap().parse().unwrap()),
-                                Type::Blob => vec.push(hex::encode(v.as_blob().unwrap())),
-                            }
-                        }
-                        res.push(vec)
-                    }
-                    None => break,
-                },
-                Err(err) => {
-                    return Err(Error::CanisterError {
-                        message: format!("{:?}", err),
-                    })
-                }
-            }
-        }
-        Ok(res)
-    })
-}
-
-fn mount_memory_files() {
-    MEMORY_MANAGER.with(|m| {
-        let m = m.borrow();
-        ic_wasi_polyfill::init_with_memory_manager(&[0u8; 32], &[], &m, 200..210);
-
-        // mount virtual memory as file for faster DB operations
-        let memory = m.get(MemoryId::new(MOUNTED_MEMORY_ID));
-        ic_wasi_polyfill::mount_memory_file(DB_FILE_NAME, Box::new(memory));
-    });
-}
-
-fn open_database() {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        *db = Some(Connection::open(DB_FILE_NAME).unwrap());
-    });
-}
-
-fn close_database() {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        *db = None;
-    });
-}
-
-fn create_tables() {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-        db.execute(
-            "CREATE TABLE IF NOT EXISTS person (
-                id    INTEGER PRIMARY KEY,
-                name  TEXT NOT NULL,
-                data  TEXT,
-                age   INTEGER
-            )",
-            (), // empty list of parameters.
-        )
-        .unwrap();
-    });
-}
-
-fn set_pragmas() {
-    // set pragmas
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-
-        // do not create and destroy the journal file every time, set its size to 0 instead
-        db.pragma_update(None, "journal_mode", &"TRUNCATE" as &dyn ToSql)
-            .unwrap();
-
-        // reduce synchronizations
-        db.pragma_update(None, "synchronous", &0 as &dyn ToSql)
-            .unwrap();
-
-        // use fewer writes to disk with larger memory chunks
-        // Note: values above 16K cause I/O errors for some reason
-        db.pragma_update(None, "page_size", &16384 as &dyn ToSql)
-            .unwrap();
-
-        // reduce locks and unlocks
-        db.pragma_update(None, "locking_mode", &"EXCLUSIVE" as &dyn ToSql)
-            .unwrap();
-
-        // temp_store = MEMORY, disables creating temp files, improves performance,
-        // this workaround also avoids sqlite error on creating a tmp file on complex queries
-        db.pragma_update(None, "temp_store", &2 as &dyn ToSql)
-            .unwrap();
-
-        // add this option to minimize disk reads and work in canister memory instead
-        //db.pragma_update(None, "cache_size", &1000000 as &dyn ToSql)
-        //    .unwrap();
-    });
-}
-
-#[ic_cdk::init]
-fn init() {
-    mount_memory_files();
-
-    open_database();
-    set_pragmas();
-    create_tables();
-}
-
-#[ic_cdk::pre_upgrade]
-fn pre_upgrade() {
-    close_database();
-}
-
-#[ic_cdk::post_upgrade]
-fn post_upgrade() {
-    mount_memory_files();
-
-    open_database();
-    set_pragmas();
+        RefCell::new(m)
+    };
 }
 
 #[derive(CandidType, Deserialize)]
@@ -202,4 +38,153 @@ enum Error {
     CanisterError { message: String },
 }
 
-type QueryResult<T = Vec<Vec<String>>, E = Error> = std::result::Result<T, E>;
+pub struct DbConnectionBuilder {
+    file_name: String,
+    db_mount_id: Option<u8>,
+    pragma_settings: HashMap<String, String>,
+}
+
+impl DbConnectionBuilder {
+    pub fn new() -> Self {
+        DbConnectionBuilder::default()
+    }
+}
+
+impl Default for DbConnectionBuilder {
+    fn default() -> Self {
+        let mut deafult_pragmas = HashMap::new();
+
+        // do not create and destroy the journal file every time, set its size to 0 instead
+        deafult_pragmas.insert("journal_mode".to_string(), "TRUNCATE".to_string());
+
+        // reduce synchronizations
+        deafult_pragmas.insert("synchronous".to_string(), "0".to_string());
+
+        // reduce locks and unlocks count
+        deafult_pragmas.insert("locking_mode".to_string(), "EXCLUSIVE".to_string());
+
+        // temp_store = MEMORY, disables creating temp files, improves performance,
+        // this workaround also avoids sqlite error on creating a tmp file on complex queries
+        deafult_pragmas.insert("temp_store".to_string(), "MEMORY".to_string());
+
+        // minimize disk reads and mostly work in canister memory instead
+        deafult_pragmas.insert("cache_size".to_string(), "1000000".to_string());
+
+        Self {
+            file_name: DB_DEFAULT_FILE_NAME.to_string(),
+            db_mount_id: Some(DEFAULT_MOUNTED_DB_ID),
+            pragma_settings: deafult_pragmas,
+        }
+    }
+}
+
+impl DbConnectionBuilder {
+    pub fn with_file_name(mut self, name: impl Into<String>) -> Self {
+        self.file_name = name.into();
+        self
+    }
+
+    pub fn with_db_mount_id(mut self, id: Option<u8>) -> Self {
+        self.db_mount_id = id;
+        self
+    }
+
+    pub fn with_pragma(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.pragma_settings.insert(key.into(), value.into());
+        self
+    }
+
+    pub fn connect(self) -> anyhow::Result<()> {
+        connect(self)
+    }
+}
+
+/// connect to a database using the connection parameters provided
+pub fn connect(connection_parameters: DbConnectionBuilder) -> anyhow::Result<()> {
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+
+        if db.is_some() {
+            return Err(anyhow::anyhow!("Database is already connected!"));
+        }
+
+        // mount DB file
+        MEMORY_MANAGER.with(|m| {
+            let m = m.borrow();
+
+            // if there is a mount id provided, create a mounted database file
+            if let Some(id) = connection_parameters.db_mount_id {
+                let memory = m.get(MemoryId::new(id));
+                ic_wasi_polyfill::mount_memory_file(
+                    &connection_parameters.file_name,
+                    Box::new(memory),
+                );
+            }
+        });
+
+        // connect database
+        *db = Some(Connection::open(connection_parameters.file_name)?);
+
+        // set pragmas
+        let conn = db.as_ref().expect("DB Connection failed!");
+
+        for (k, v) in &connection_parameters.pragma_settings {
+            if !v.is_empty() {
+                conn.pragma_update(None, k, v)?;
+            }
+        }
+
+        Ok(())
+    })
+}
+
+/// close database connection
+pub fn disconnect() -> anyhow::Result<()> {
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+
+        if let Some(conn) = db.take() {
+            // Try to close the connection if it is open
+            conn.close().map_err(|(_conn, err)| anyhow::anyhow!(err))?;
+
+            // close the connection and free the resources
+            *db = None;
+
+            // unmount the corresponding file
+            DB_FILE_NAME.with(|name| {
+                let name = name.borrow();
+
+                if let Some(name) = name.as_ref() {
+                    // also close the db mount
+                    ic_wasi_polyfill::unmount_memory_file(name);
+                }
+            })
+        }
+
+        Ok(())
+    })
+}
+
+pub fn execute<P: rusqlite::Params>(sql: &str, p: P) -> rusqlite::Result<usize> {
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+        let db = db.as_mut().unwrap();
+        db.execute(sql, p)
+    })
+}
+
+pub fn query<P, F, T>(sql: String, params: P, mut f: F) -> rusqlite::Result<Vec<T>>
+where
+    P: rusqlite::Params,
+    F: FnMut(&rusqlite::Row) -> rusqlite::Result<T>,
+{
+    DB.with(|db| {
+        let mut db = db.borrow_mut();
+        let db = db.as_mut().unwrap();
+
+        let mut stmt = db.prepare(&sql)?;
+        let iter = stmt.query_map(params, |row| f(row))?;
+
+        iter.collect()
+    })
+}
